dbb9a5bff16064b03c97bac6fe2b899fc631fd5e
Andre <30847673+andgtr08@users.noreply.github.com>
2017-11-01 21:55:07 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Curso(models.Model):
     nome = models.CharField(max_length=200)
     periodo = models.CharField(max_length=50)
-    instituicao = models.CharField(max_length=200)
+    instituição = models.CharField(max_length=200)
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
